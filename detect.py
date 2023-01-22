93a8b15ba3ecec424267ed093a74429a20f34dea
Filmimagine <B6217570@g.sut.ac.th>
2023-01-22 13:52:28 +0700

    show im1

--- detect.py
+++ detect.py
@@ -22,7 +22,7 @@ from utils.general import LOGGER,check_img_size, check_requirements, check_imsho
     scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
 from utils.plots import plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_synchronized
-# from utils.datasets import LoadWebcam
+from utils.datasets import LoadWebcam
 
 def takeSecond(elem):
     return elem[1]
